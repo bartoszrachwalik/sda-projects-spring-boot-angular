2733b87b21f340856680147f5a6c2ea05bf005c6
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-21 20:42:54 +0100

    adds components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PlayerComponent } from './player/player.component';
+import { PlayerEditComponent } from './player-edit/player-edit.component';
+import { PlayerDetailComponent } from './player-detail/player-detail.component';
+import { PlayerCreateComponent } from './player-create/player-create.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerComponent,
+    PlayerEditComponent,
+    PlayerDetailComponent,
+    PlayerCreateComponent
   ],
   imports: [
     BrowserModule
