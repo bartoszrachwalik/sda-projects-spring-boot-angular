d268587524c5ddc7ece654ea01f4b893c670ecf4
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-21 20:19:17 +0100

    adds PlayerService and TeamRepository

---
+++ src/main/java/com/github/springboot/angular/player/application/TeamRepository.java
@@ -0,0 +1,12 @@
+package com.github.springboot.angular.player.application;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+interface TeamRepository extends JpaRepository<TeamEntity, Integer> {
+    Optional<TeamEntity> findByName(String name);
+}
+
